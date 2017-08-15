c913a45291b300ddce01d3156115afeba3364541
Justin Lee <justinlee0022@gmail.com>
2017-08-15 14:52:52 -0700

    Small changes

--- Home/home.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function ($) {
-    //$("#container").prepend($("#main-content"));
-    $("div[role='main']").prepend($("#main-content"));
-    $("#main-content").prepend($("#content"));
-    $("#container").prepend($(".image-region"));
-});
